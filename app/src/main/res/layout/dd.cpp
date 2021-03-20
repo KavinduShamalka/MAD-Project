ac8796a453021e33cb658fc9b518099b9f41c14d
Sithum-Ravishka <79629419+Sithum-Ravishka@users.noreply.github.com>
2021-03-20 13:32:48 +0530

    Commit

---
+++ app/src/main/res/layout/dd.cpp
@@ -0,0 +1,5 @@
+//
+// Created by user on 3/20/2021.
+//
+
+#include "dd.h"
---
+++ app/src/main/res/layout/dd.h
@@ -0,0 +1,16 @@
+//
+// Created by user on 3/20/2021.
+//
+
+#ifndef CODEBLACK_DD_H
+#define CODEBLACK_DD_H
+
+
+
+class dd {
+
+};
+
+
+
+#endif //CODEBLACK_DD_H
