e1df2468725917263de876e2794eb4fd90bba941
KavindduShamalka <kavindusuraweera27@gmail.com>
2021-03-21 13:32:03 +0530

    Wlcomepages java

---
+++ app/src/main/java/com/example/codeblack/Wlcomepage.java
@@ -0,0 +1,14 @@
+package com.example.codeblack;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Wlcomepage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wlcomepage);
+    }
+}
