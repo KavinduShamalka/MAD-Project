879bc1417590360cc55ddfe783352e66ecaa8104
user <charithdhananjya0701@gmail.com>
2021-03-21 13:03:52 +0530

    main change button

---
+++ app/src/main/java/com/example/codeblack/VehicleAddCreation2.java
@@ -0,0 +1,14 @@
+package com.example.codeblack;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VehicleAddCreation2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vehicle_add_creation2);
+    }
+}
